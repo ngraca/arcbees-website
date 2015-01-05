761c5aee6c3c582386d0a7d9a77edc8ddd07c1ab
larrymatte <matte.larry@gmail.com>
2015-01-05 14:02:01 -0500

    page bees text

--- src/main/java/com/arcbees/website/client/application/bees/BeesModule.java
+++ src/main/java/com/arcbees/website/client/application/bees/BeesModule.java
@@ -17,12 +17,38 @@
 package com.arcbees.website.client.application.bees;
 
 import com.arcbees.website.client.application.bees.bee.ChristianModule;
+import com.arcbees.website.client.application.bees.bee.Christopher.ChristopherModule;
+import com.arcbees.website.client.application.bees.bee.Genevieve.GenevieveModule;
+import com.arcbees.website.client.application.bees.bee.Jason.JasonModule;
+import com.arcbees.website.client.application.bees.bee.Jeanchristophe.JeanchristopheModule;
+import com.arcbees.website.client.application.bees.bee.Joel.JoelModule;
+import com.arcbees.website.client.application.bees.bee.Julien.JulienModule;
+import com.arcbees.website.client.application.bees.bee.Larry.LarryModule;
+import com.arcbees.website.client.application.bees.bee.Manon.ManonModule;
+import com.arcbees.website.client.application.bees.bee.Maxime.MaximeModule;
+import com.arcbees.website.client.application.bees.bee.PhilippeA.PhilippeAModule;
+import com.arcbees.website.client.application.bees.bee.PhilippeB.PhilippeBModule;
+import com.arcbees.website.client.application.bees.bee.Simonpierre.SimonpierreModule;
+import com.arcbees.website.client.application.bees.bee.Francois.FrancoisModule;
 import com.gwtplatform.mvp.client.gin.AbstractPresenterModule;
 
 public class BeesModule extends AbstractPresenterModule {
     @Override
     protected void configure() {
         install(new ChristianModule());
+        install(new FrancoisModule());
+        install(new PhilippeBModule());
+        install(new GenevieveModule());
+        install(new PhilippeAModule());
+        install(new JulienModule());
+        install(new ManonModule());
+        install(new MaximeModule());
+        install(new SimonpierreModule());
+        install(new ChristopherModule());
+        install(new JoelModule());
+        install(new JeanchristopheModule());
+        install(new JasonModule());
+        install(new LarryModule());
 
         bindPresenter(BeesPresenter.class, BeesPresenter.MyView.class,
                 BeesView.class, BeesPresenter.MyProxy.class);
